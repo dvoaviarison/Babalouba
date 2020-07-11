69940ff7e05033686b9f79c9b06d1f99f7288be3
Daniel Voaviarison <daniel.voaviarison@na.linedata.com>
2020-07-10 22:17:49 -0400

    More cleanup

--- Babalouba/PlayerAPI/MCINotify.cs
+++ Babalouba/PlayerAPI/MCINotify.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: PlayerAPI.MCINotify
-// Assembly: BABALOUBA, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 36344566-A3BA-419C-9F40-383BB3344FF3
-// Assembly location: C:\Users\Dvoaviarison\Downloads\BABALOUBA 1.1.exe
-
-namespace PlayerAPI
+﻿namespace PlayerAPI
 {
   public enum MCINotify
   {
